88d8e5ae76f49b639c1e0ea123af31b412ca8902
fitzrovian <fitzrovian@3adbc9d6-a949-0410-9cc0-931bda5f43ff>
2009-07-07 11:31:56 +0000

    Minor change to contacts email stuff to close the contacts views earlier.
    git-svn-id: https://mobbler.googlecode.com/svn/trunk@746 3adbc9d6-a949-0410-9cc0-931bda5f43ff

--- src/mobblercontacts.cpp
+++ src/mobblercontacts.cpp
@@ -37,14 +37,6 @@ CMobblerContacts::CMobblerContacts()
 
 CMobblerContacts::~CMobblerContacts()
 	{
-	if(iFilteredView)
-		{
-		iFilteredView->Close(*this);
-		}
-	if(iRemoteView)
-		{
-		iRemoteView->Close(*this);
-		}
 	delete iNameList;
 	delete iEmailList;
 	delete iDb;
@@ -126,6 +118,8 @@ void CMobblerContacts::BuildListsL()
 		CleanupStack::PopAndDestroy(nameBuf);
 		iEmailList->AppendL(contact.Field(EEmail));
 		}
+	iFilteredView->Close(*this);
+	iRemoteView->Close(*this);
 	}
 
 void CMobblerContacts::HandleContactViewEvent(const CContactViewBase& aView, const TContactViewEvent& aEvent)
