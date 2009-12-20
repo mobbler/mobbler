5e5a8b3ba2f2d07fc9e7dbd33151bb4f7bc796ae
gw111zz <gw111zz@users.noreply.github.com>
2009-12-20 18:41:42 +0000

    Fix encoding mismatch

--- src/mobblerbrowsercontrol.cpp
+++ src/mobblerbrowsercontrol.cpp
@@ -62,7 +62,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * %S - bio
  */
 
-_LIT8(KArtistInfoHtmlTemplate, "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\
+_LIT8(KArtistInfoHtmlTemplate, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\
 	<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\
 	<html xmlns=\"http://www.w3.org/1999/xhtml\">\
 \
