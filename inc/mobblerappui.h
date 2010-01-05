fde5751f8eb36c6aa4825a48915c2df9978d9a10
Michael Coffey <eartle@users.noreply.github.com>
2010-01-05 12:25:51 +0000

    Fixed a problem with non-subscriber radio and updated the build number to 241.

--- inc/mobblerappui.h
+++ inc/mobblerappui.h
@@ -46,7 +46,7 @@ const TInt KUpdateIntervalDays(7);
 const TInt KMaxMobblerTextSize(255);
 
 const TInt KMobblerMinorVersion(8);
-const TInt KMobblerBuildNumber(237);
+const TInt KMobblerBuildNumber(241);
 
 #ifdef __SYMBIAN_SIGNED__
 const TVersion KVersion(1, KMobblerMinorVersion, KMobblerBuildNumber);
