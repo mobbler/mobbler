2fdfb44c5359584a72ffb4b0840105d17d41be79
Michael Coffey <eartle@users.noreply.github.com>
2010-01-19 14:33:02 +0000

    Enable music player controlling for 3.1 and 3.2 phones in signed builds.

--- musicappobserver/src/musicappobserver.cpp
+++ musicappobserver/src/musicappobserver.cpp
@@ -258,22 +258,32 @@ TMobblerMusicAppObserverCommand CMobblerMusicAppObserver::ConvertCommand(TMPlaye
 
 TBool CMobblerMusicAppObserver::ControlsSupported()
 	{
+#ifdef __SYMBIAN_SIGNED__
+	return ETrue;
+#else
 	return EFalse;
+#endif
 	}
 
 void CMobblerMusicAppObserver::PlayL()
 	{
-	
+#ifndef __WINS__
+	User::LeaveIfError(iEngine->HandleCommand(EMPlayerRCtrlCmdPlay));
+#endif
 	}
 
 void CMobblerMusicAppObserver::StopL()
 	{
-	
+#ifndef __WINS__
+	User::LeaveIfError(iEngine->HandleCommand(EMPlayerRCtrlCmdStop));
+#endif
 	}
 
 void CMobblerMusicAppObserver::SkipL()
 	{
-	
+#ifndef __WINS__
+	User::LeaveIfError(iEngine->HandleCommand(EMPlayerRCtrlCmdNextTrack));
+#endif
 	}
 
 // End of file
