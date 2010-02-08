62478cc3ea9abfe8f4d31981093a0b130f9ef535
hugovk <hugovk@localhost>
2010-02-08 20:25:53 +0200

    Forgot to add mobblerliterals.cpp yesterday.

---
+++ src/mobblerliterals.cpp
@@ -0,0 +1,62 @@
+/*
+mobblerliterals.cpp
+
+Mobbler, a Last.fm mobile scrobbler for Symbian smartphones.
+Copyright (C) 2010  Michael Coffey
+
+http://code.google.com/p/mobbler
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <e32std.h>
+#include "mobblerliterals.h"
+
+GLDEF_D const TPtrC8 K0(_S8("0"));
+GLDEF_D const TPtrC8 K1(_S8("1"));
+GLDEF_D const TPtrC8 KAlbum(_S8("album"));
+GLDEF_D const TPtrC8 KArtist(_S8("artist"));
+GLDEF_D const TPtrC8 KDescription(_S8("description"));
+GLDEF_D const TPtrC8 KError(_S8("error"));
+GLDEF_D const TPtrC8 KEvent(_S8("event"));
+GLDEF_D const TPtrC8 KEvents(_S8("events"));
+GLDEF_D const TPtrC8 KFriends(_S8("friends"));
+GLDEF_D const TPtrC8 KGetFriends(_S8("getfriends"));
+GLDEF_D const TPtrC8 KGetInfo(_S8("getinfo"));
+GLDEF_D const TPtrC8 KGetPlaylists(_S8("getplaylists"));
+GLDEF_D const TPtrC8 KId(_S8("id"));
+GLDEF_D const TPtrC8 KImage(_S8("image"));
+GLDEF_D const TPtrC8 KLarge(_S8("large"));
+GLDEF_D const TPtrC8 KMbid(_S8("mbid"));
+GLDEF_D const TPtrC8 KName(_S8("name"));
+GLDEF_D const TPtrC8 KOk(_S8("ok"));
+GLDEF_D const TPtrC8 KPlaylists(_S8("playlists"));
+GLDEF_D const TPtrC8 KSearch(_S8("search"));
+GLDEF_D const TPtrC8 KSize(_S8("size"));
+GLDEF_D const TPtrC8 KStation(_S8("station"));
+GLDEF_D const TPtrC8 KStatus(_S8("status"));
+GLDEF_D const TPtrC8 KTag(_S8("tag"));
+GLDEF_D const TPtrC8 KTags(_S8("tags"));
+GLDEF_D const TPtrC8 KTitle(_S8("title"));
+GLDEF_D const TPtrC8 KTrack(_S8("track"));
+GLDEF_D const TPtrC8 KUser(_S8("user"));
+GLDEF_D const TPtrC8 KVenue(_S8("venue"));
+
+GLDEF_D const TPtrC8 KEquals(_S8("="));
+
+//GLDEF_D const TPtrC KLogFile(_S("c:\\Data\\Mobbler\\.scrobbler.log"));
+GLDEF_D const TPtrC KMapKmlFilename(_S("C:\\System\\Data\\Mobbler\\map.kml"));
+
+// End of file
